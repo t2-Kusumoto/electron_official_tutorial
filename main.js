702ddd61658f23169eb03b86d9e81b1c23b22073
Tetsuya Kusumoto <kusumot2.ya.o@gmail.com>
2022-08-10 09:33:17 +0900

    add Event Listener main.js

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
 });
